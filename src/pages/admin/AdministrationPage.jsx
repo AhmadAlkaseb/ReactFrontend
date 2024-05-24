6a371306c5f310369756a743bfa1306e2d075797
HanniSalman <hannisalman2510@outlook.com>
2024-05-24 12:17:01 +0200

    .

--- src/pages/admin/AdministrationPage.jsx
+++ src/pages/admin/AdministrationPage.jsx
@@ -1,12 +1,29 @@
+import React, { useState } from 'react';
 import UserList from "./UserList.jsx";
+import AdminUserInputForm from "./AdminUserInputForm.jsx";
 
 const AdministrationPage = () => {
+    const [updateUser, setUpdateUser] = useState(false);
+    const [user, setUser] = useState({
+        email: '',
+        role: ''
+    });
 
     return (
-        <>
-            <UserList />
+        <>     
+            <AdminUserInputForm 
+                updateUser={updateUser} 
+                setUpdateUser={setUpdateUser} 
+                user={user} 
+                setUser={setUser} 
+            />   
+            <UserList 
+                updateUser={updateUser} 
+                setUpdateUser={setUpdateUser} 
+                setUser={setUser} 
+            />
         </>
     );
 };
 
-export default AdministrationPage
+export default AdministrationPage;
